9d6e3e0f300e5aff9656ad7ce363032638234335
isuru1221 <isurusandaruwann1221@gmail.com>
2019-09-01 16:29:58 +0530

    create login page and route

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>Login Page</h3>
+
+    </div>
+</template>
+
+<script>
+   import firebase from 'firebase';
+   export default {
+        name: 'login',
+        methods: {
+            login(){
+                const provider = new firebase.auth.GoogleAuthProvider();
+                provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+                firebase.auth().signInWithPopup(provider).then( (result)=>{
+                    let token = result.credential.accessToken;
+                    let user = result.user;
+                    this.$router.push('/');
+                }).catch((error)=>{
+                    console.log("error message" + error.message);
+                })
+            }
+        }
+    }
+</script>
