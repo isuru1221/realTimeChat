ed52f1d036bbed4ddcc342dd5bb5ab8bb3b7ed4b
isuru1221 <isurusandaruwann1221@gmail.com>
2019-09-01 13:54:26 +0530

    add & config firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+require('firebase/firestore');
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
+  databaseURL: "https://"+process.env.VUE_APP_FIREBASE_PROJECT_ID+".com",
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
+  messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+});
+const db = firebase.firestore();
+window.db = db;
 
 new Vue({
   router,
